8a2942c05656536cb713463a7749435f4a2f8461
zllrunning <zllrunning@gmail.com>
2019-07-22 09:53:52 +0800

    add __init__.py

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Demo')
 parser.add_argument('--resume', default='cp/SiamMask_DAVIS.pth', type=str,
                     metavar='PATH', help='path to latest checkpoint (default: none)')
 parser.add_argument('--data', default='data/Human6', help='videos or image files')
-parser.add_argument('--mask-dilation', default=24, type=int, help='mask dilation when inpainting')
+parser.add_argument('--mask-dilation', default=32, type=int, help='mask dilation when inpainting')
 args = parser.parse_args()
 
 mask(args)
